39d058b2f4c497541e089bc2405c1ac85db349c8
Peter Opeyemi James <mail2peterjames@gmail.com>
2025-12-01 16:45:14 +0000

    Fix: explicitly instantiate AzuressoController in admin & site entry files (resolves resolver errors)

--- admin/azuresso.php
+++ admin/azuresso.php
@@ -3,6 +3,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\Component\Azuresso\Administrator\Controller\AzuressoController;
 
 // Load vendor autoload if available
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
@@ -12,5 +13,13 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 $input = Factory::getApplication()->input;
 $task = $input->getCmd('task', 'display');
 
-$controller = BaseController::getInstance('Azuresso');
-$controller->execute($task);
+// Try to instantiate controller explicitly so Joomla's resolver isn't relied upon
+try {
+    $controller = new AzuressoController();
+    $controller->execute($task);
+    $controller->redirect();
+} catch (\Throwable $e) {
+    // Fallback to the default resolver (keeps backwards compatibility)
+    $controller = BaseController::getInstance('Azuresso');
+    $controller->execute($task);
+}
--- site/azuresso.php
+++ site/azuresso.php
@@ -3,6 +3,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\Component\Azuresso\Site\Controller\AzuressoController;
 
 // Load vendor autoload if available
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
@@ -12,6 +13,14 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 $input = Factory::getApplication()->input;
 $task = $input->getCmd('task', 'display');
 
-$controller = BaseController::getInstance('Azuresso');
-$controller->execute($task);
-$controller->redirect();
+// Try to instantiate controller explicitly (avoid resolver issues)
+try {
+    $controller = new AzuressoController();
+    $controller->execute($task);
+    $controller->redirect();
+} catch (\Throwable $e) {
+    // Fallback to default resolver
+    $controller = BaseController::getInstance('Azuresso');
+    $controller->execute($task);
+    $controller->redirect();
+}
